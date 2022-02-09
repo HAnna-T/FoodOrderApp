bed52b2928f6f084686229863346df3d25aba8ec
Anna Harutyunyan <anuanharutyunyan>
2022-02-09 11:08:58 +0400

    feat: Working with Refs and Forwarding Refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 import styles from "./MealItem.module.css";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+  const addItemToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
   return (
     <li className={styles.meal}>
       <div>
@@ -12,7 +22,7 @@ const MealItem = (props) => {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-          <MealItemForm/>
+        <MealItemForm onAddToCart={addItemToCartHandler} />
       </div>
     </li>
   );
